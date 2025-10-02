9fc2ceaac800b9955d37b1d74c41751f7d215954
Ignasia Agata <ignasiaagata68@gmail.com>
2025-10-02 15:33:52 +0700

    CRUD

---
+++ usermgmt/update_user.php
@@ -0,0 +1,32 @@
+<?php
+//get data from the edit form
+$userID = $_POST['id'];
+$fname = $_POST['fname'];
+$role = $_POST['role'];
+
+
+// echo "Product name:" , $prodName, "<br>";
+// echo "Product description: " , $prodDesc, "<br>";
+// echo "Product price: " , $prodPrice, "<br><br>";
+//create connection
+include '../connect.php';
+
+//update data
+$sql = "UPDATE users 
+        SET fullname = '$fname',
+        role = '$role',
+        WHERE id = $userID";
+
+// VALUES ('$_POST[name]', '$_POST[desc]', '$_POST[price]')";
+
+//check if data is inserted successfully
+if ($conn->query($sql) === TRUE) {
+  // echo "New record created successfully";
+  // redirect to file read_one (view data)
+  header('Location: view_account.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
